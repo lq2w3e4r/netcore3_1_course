e9615a219768cb6e1cea4c1cda73eb819bf0de71
lq2w3e4r <jjt8989@gmail.com>
2020-04-01 15:55:51 +0900

    Blazor testing work

---
+++ RazorPagesMovie/Controllers/ProductsaaaController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using RazorPagesMovie.Models;
+using RazorPagesMovie.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorPagesMovie.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult GetAction(
+            [FromQuery] string ProductId, 
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
